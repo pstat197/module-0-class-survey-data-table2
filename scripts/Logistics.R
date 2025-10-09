571af3eed16d0e29ecacd9532ccf62494f8e27d2
lorrettalu <lu10@ucsb.edu>
2025-10-09 16:35:05 -0700

    Added in data set

---
+++ scripts/Logistics.R
@@ -0,0 +1,9 @@
+# Cathy and Lorretta
+# Logistics Regression
+library(here)
+
+# set folder to where data is saved
+folder <- 'data'
+
+# read in csv
+logistics <- read_csv(here(folder, 'background-clean.csv'))
